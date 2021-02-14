81cc4ea982f91bd9e7a2223c42e77d504e8da581
hlin91 <hlin91@outlook.com>
2021-02-13 22:57:55 -0800

    Added mapbox component

---
+++ src/components/Map.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-col v-bind:class="cols" height="100%" id="map">
+  </v-col>
+</template>
+
+<script>
+import mapboxgl from 'mapbox-gl'
+
+export default {
+    props:['cols'],
+    
+    head () {
+        return  {
+            script: [
+                { src: 'https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.js' }
+            ],
+            link: [
+                { 
+                    href: 'https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css',
+                    rel: 'stylesheet'
+                }
+            ]
+        }
+    },
+    methods: {
+        map: function() {
+            // Creates and adds a mapbox to the element with id "map"
+            mapboxgl.accessToken = 'pk.eyJ1IjoiaGxpbjkxIiwiYSI6ImNrbDQ2MjY4NzE0ZXEycHFpaXBya2tvN3gifQ.Tqa8iLUqXeKZQ8SmhLoRtg';
+            
+            return new mapboxgl.Map({
+                container: 'map',
+                center: [-74.5, 40],
+                zoom: 9,
+                style: 'mapbox://styles/mapbox/streets-v11'
+            });
+        }
+    },
+    data: {
+        function() {
+            return {
+                map: null
+            }
+        }
+    },
+    mounted() {
+        map = this.map()
+        map.addControl(new mapboxgl.NavigationControl());
+    },
+    template: '<v-col :cols={{ cols }} height="100%" id="map"></v-col>'
+}
+</script>
+
+<style>
+  .map {
+    height: 100%;
+    width: 100%;
+  }
+</style>
