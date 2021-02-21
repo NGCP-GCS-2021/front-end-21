de1c5bb379e8c06f280d9342d33b50e7cb5c6cc1
hlin91 <hlin91@outlook.com>
2021-02-20 23:52:57 -0800

    Added WIP implementation of addCircle()

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -25,6 +25,7 @@ export default {
     methods: {
         makeMap: function() {
             // Creates and adds a mapbox to the element with id "map"
+            // TODO: Add support for passing in start coordinates, zoom level, and bounds
             mapboxgl.accessToken = 'pk.eyJ1IjoiaGxpbjkxIiwiYSI6ImNrbDQ2MjY4NzE0ZXEycHFpaXBya2tvN3gifQ.Tqa8iLUqXeKZQ8SmhLoRtg';
             
             return new mapboxgl.Map({
@@ -34,16 +35,41 @@ export default {
                 style: 'mapbox://styles/mapbox/streets-v11'
             });
         }
+        
+        addCircle: function(latitude, longitude, radius, name, color) {
+            // Adds a circle to the map at the specified coordinate
+            // With specified radius, name, and color
+            this.map.addSource(name, {
+                'type': 'geojson',
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Point'
+                    'coordinates': [latitude, longitude]
+                }
+            });
+            this.map.addLayer({
+                'id': name + '_layer',
+                'type': 'circle',
+                'source': name,
+                'paint': {
+                    'circle-radius': {
+                        'base': radius
+                        // Can add "stops" to support scaling when zoomed in/out
+                    },
+                    'circle-color': color
+                }
+            });
+        }
     },
     data: {
         function() {
             return {
                 map: null
-            }
+            };
         }
     },
     mounted() {
-        map = this.makeMap()
+        map = this.makeMap();
         map.addControl(new mapboxgl.NavigationControl());
     },
     template: '<v-col :cols={{ cols }} height="100%" id="map"></v-col>'
