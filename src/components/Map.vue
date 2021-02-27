2091ad40bb2c19ee8657763221a0b0d40c26b7b0
hlin91 <hlin91@outlook.com>
2021-02-26 20:17:44 -0800

    Added support for passing bounds and addCoord function

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -22,6 +22,22 @@ export default {
         zoom: {
             default: 9,
             type: Number
+        },
+        SW_bound_lat: {
+            default: null,
+            type: Number
+        },
+        SW_bound_long: {
+            default: null,
+            type: Number
+        },
+        NE_bound_lat: {
+            default: null,
+            type: Number
+        },
+        NE_bound_long: {
+            default: null,
+            type: Number
         }
     },
     
@@ -41,21 +57,37 @@ export default {
     methods: {
         makeMap: function() {
             // Creates and adds a mapbox to the element with id "map"
-            // TODO: Add support for passing in start coordinates, zoom level, and bounds
             mapboxgl.accessToken = 'pk.eyJ1IjoiaGxpbjkxIiwiYSI6ImNrbDQ2MjY4NzE0ZXEycHFpaXBya2tvN3gifQ.Tqa8iLUqXeKZQ8SmhLoRtg';
-            
-            return new mapboxgl.Map({
-                container: 'map',
-                center: [this.center_long, this.center_lat],
-                zoom: this.zoom,
-                style: 'mapbox://styles/mapbox/satellite-streets-v11'
-            });
+            if (this.SW_bound_lat != null && this.SW_bound_long != null
+                && this.NE_bound_lat != null && this.NE_bound_long != null) {
+                // User supplied a valid bound
+                var bounds = [
+                    [this.SW_bound_long, this.SW_bound_lat],
+                    [this.NE_bound_long, this.NE_bound_lat]
+                ];
+                return new mapboxgl.Map({
+                    container: 'map',
+                    center: [this.center_long, this.center_lat],
+                    zoom: this.zoom,
+                    style: 'mapbox://styles/mapbox/satellite-streets-v11',
+                    maxBounds: bounds
+                });
+            } else {
+                // Create a map with no bounds
+                return new mapboxgl.Map({
+                    container: 'map',
+                    center: [this.center_long, this.center_lat],
+                    zoom: this.zoom,
+                    style: 'mapbox://styles/mapbox/satellite-streets-v11'
+                });
+            }
         },
         
-        addCircle: function(latitude, longitude, radius, name, color) {
+        addCircle: function(longitude, latitude, radius, name, color) {
+            // TODO: Test this
             // Adds a circle to the map at the specified coordinate
             // With specified radius in METERS, name, and color
-
+            
             // Used to set the radius for max zoom level 22 so circle scales accurately with zoom
             const metersToPixelsAtMaxZoom = (meters, latitude) =>
                   meters / (78271.484 / 2 ** 22) / Math.cos(latitude * Math.PI / 180)
@@ -83,6 +115,28 @@ export default {
                     'circle-color': color
                 }
             });
+        },
+        
+        addCoord: function(lng, lat) {
+            // Taken from CurrentMap.html
+            var point = new mapboxgl.Point(lng, lat);
+            var marker = new mapboxgl.Marker()
+                .setLngLat([lng,lat])
+                .setPopup(new mapboxgl.Popup().setHTML(lng + ',' + lat))
+                .addTo(this.map); // add the marker to the map
+            
+            return {
+                center: [lng, lat],
+                geometry: {
+                    type: 'Point',
+                    coordinates: [lng, lat]
+                },
+                
+                place_name: 'Lat: ' + lat + ' Lng: ' + lng,
+                place_type: ['coordinate'],
+                properties: {},
+                type: 'Feature'
+            };
         }
     },
     data: {
