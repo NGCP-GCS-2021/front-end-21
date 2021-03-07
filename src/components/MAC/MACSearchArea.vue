7f924115d7b3f55e682280116b93b89503414a4d
Matthew Le <mattmattmattle@gmail.com>
2021-03-06 20:09:04 -0800

    Update MACSearchArea.vue
    - fully merged MAC search area changes

--- src/components/MAC/MACSearchArea.vue
+++ src/components/MAC/MACSearchArea.vue
@@ -1,80 +1,30 @@
 <template>
   <div>
-    <h1 class="font-weight-light" pa-0 ma-0>Search Area</h1>
-    <validation-observer ref="observer" v-slot="{ invalid }">
-      <form
-        class="scrollable"
-        @submit.prevent="submit"
-        style="height: 250px; overflow-y: hidden; overflow-x: hidden"
-      >
-        <v-container>
-          <v-form v-for="(input, k) in Coordinates" :key="k">
-            <v-row>
-              <v-col cols="5">
-                <validation-provider
-                  v-slot="{ errors }"
-                  name="Latitude"
-                  :rules="{
-                    required: true,
-                  }"
-                  class="pa-0 ma-0"
-                >
-                  <v-text-field
-                    v-model="input.lat"
-                    :error-messages="errors"
-                    label="Latitude"
-                    required
-                  ></v-text-field>
-                </validation-provider>
-              </v-col>
-              <v-col cols="5">
-                <validation-provider
-                  v-slot="{ errors }"
-                  name="Longitude"
-                  :rules="{
-                    required: true,
-                  }"
-                >
-                  <v-text-field
-                    v-model="input.lng"
-                    :error-messages="errors"
-                    label="Longitude"
-                    required
-                  ></v-text-field>
-                </validation-provider>
-              </v-col>
-              <v-col :cols="1">
-                <v-icon
-                  class="mt-2"
-                  color="green"
-                  @click="add(k)"
-                  v-show="k == Coordinates.length - 1"
-                  >mdi-plus-circle</v-icon
-                >
-              </v-col>
-              <v-col :cols="1">
-                <v-icon
-                  class="mt-2"
-                  color="red"
-                  @click="remove(k)"
-                  v-show="k || (!k && Coordinates.length > 1)"
-                  >mdi-minus-circle</v-icon
-                >
-              </v-col>
-            </v-row>
-          </v-form>
-          <v-row>
-            <v-btn class="mr-4" color="green" type="submit" :disabled="invalid">
-              Submit
-            </v-btn>
-            <v-btn @click="dialog = true"> Clear </v-btn>
-            <!-- <div>
-              <h3>Coordinates: {{ Coordinates }}</h3>
-            </div> -->
-          </v-row>
-        </v-container>
-      </form>
-    </validation-observer>
+    <v-container>
+      <v-row>
+        <v-col>
+          <h1 class="font-weight-light" pa-0 ma-0>Search Area</h1>
+        </v-col>
+        <v-col align="center">
+          <v-container>
+          <PolygonToggle @selected="selectShape"/>
+          </v-container>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container v-if="shape === 'polygon'">
+      <v-row >
+        <PolygonForm/>
+      </v-row>
+    </v-container>
+
+    <v-container v-if="shape === 'circle'">
+      <v-row >
+        <CircleForm/>
+      </v-row>
+    </v-container>
+
     <v-dialog v-model="dialog" max-width="425">
       <v-card>
         <v-card-title class="headline">
