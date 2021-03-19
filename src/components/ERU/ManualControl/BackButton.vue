255ad6880ee7475a35ccace0b7e753f0108047f6
Matthew Le <mattmattmattle@gmail.com>
2021-03-19 15:20:48 -0700

    Centering Buttons

--- src/components/ERU/ManualControl/BackButton.vue
+++ src/components/ERU/ManualControl/BackButton.vue
@@ -1,5 +1,7 @@
 <template>
-  <v-btn class="mx-2" fab dark color="gray">
-    <v-icon dark> mdi-arrow-left </v-icon>
-  </v-btn>
+  <v-card-actions class="justify-left">
+    <v-btn class="mx-2" fab dark color="gray">
+      <v-icon dark> mdi-arrow-left </v-icon>
+    </v-btn>
+  </v-card-actions>
 </template>
--- src/components/ERU/ManualControl/PowerButton.vue
+++ src/components/ERU/ManualControl/PowerButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card-actions class="justify-left">
+  <v-card-actions class="justify-right">
     <v-btn class="mx-2" fab dark color="green">
       <v-icon dark> mdi-power-standby </v-icon>
     </v-btn>
