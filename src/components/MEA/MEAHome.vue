a2cfbd7a161514fc57d6b5eb4d75b1cb40622618
Feng Xie <fengxie.010@gmail.com>
2021-04-16 15:47:39 -0700

    Update MEAHome.vue

--- src/components/MEA/MEAHome.vue
+++ src/components/MEA/MEAHome.vue
@@ -97,15 +97,84 @@ export default {
     ValidationProvider,
     ValidationObserver,
   },
-  data() {
-    return {
-      travelTo: {},
-      Latitude: "",
-      Longitude: "",
-    };
-  },
-
+  data: () => ({
+    menu: false,
+    //All stage id's are: (Integer Indication + 1)
+    currentStage: {
+      stage: null,
+      id: -1,
+    },
+    stages: [
+      {
+        stage: "Ready to Start",
+        id: 1,
+      },
+      {
+        stage: "Takeoff to Minimum Altitude",
+        id: 2,
+      },
+      {
+        stage: "Drive to Hiker",
+        id: 3,
+      },
+      {
+        stage: "ERU Drop",
+        id: 4,
+      },
+      {
+        stage: "Return to Home/Travel to Position",
+        id: 10,
+      },
+    ],
+    currentData: null,
+  }),
+mounted() {
+  this.getCurrentTravelTo();
+  this.getCurrentStage();
+},
   methods: {
+    getCurrentTravelTo(){
+      const path = "http://127.0.0.1:5000/MEA_INPUT";
+      axios
+          .get(path)
+          .then((res) => {
+            this.Longitude = res.data.Travel_to_lng;
+            this.Latitude = res.data.Travel_to_lat;
+            this.Altitude = res.data.Travel_to_alt;
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+    },
+    getCurrentStage() {
+      const path = "http://127.0.0.1:5000/MEA_XBEE";
+
+      axios
+          .get(path)
+          .then((res) => {
+            this.currentData = res.data.MAC;
+            this.setCurrentStage();
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+    },
+    setCurrentStage(){
+      for (let i = 0; i < this.currentData.length; i++) {
+        let pair = this.currentData[i];
+        if (pair.title == "Current_stage") {
+          this.currentStage.id = pair.value;
+
+          for (let k = 0; k < this.stages.length; k++) {
+            if (this.currentStage.id == this.stages[k].id) {
+              this.currentStage.stage = this.stages[k].stage;
+              i = this.currentData.length; //ends loop
+              k = this.stages.length; //ends loop
+            }
+          }
+        }
+      }
+    },
     submit() {
       this.$refs.observer.validate();
       this.postTravelTo();
