9a8c41b7477ad3e439add5a6d7be7bec2bdab1aa
Maximum Wilder-Smith <maxwildersmith@gmail.com>
2021-04-30 14:15:31 -0700

    Update MACStatus.vue

--- src/components/MAC/MACStatus.vue
+++ src/components/MAC/MACStatus.vue
@@ -72,7 +72,7 @@ export default {
           var newarray = JSON.parse(JSON.stringify( res.data.MAC, newOrderArray));
           this.mac_data = newarray;
 
-          console.log("getting data....")
+          // console.log("getting data....")
           this.mac_data = res.data.MAC;
           // console.log("getting data....")
           this.setCurrentStage();
