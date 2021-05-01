9c839facc5c9757f51ca0eeae7eb1744e1afba62
Feng Xie <fengxie.010@gmail.com>
2021-05-01 13:25:16 -0700

    added a progress bar

--- src/views/MAC.vue
+++ src/views/MAC.vue
@@ -16,6 +16,15 @@
         />
         <v-col :cols="6">
           <v-container fluid flex>
+            <v-row id="progressBar">
+              <v-progress-linear indeterminate color="green"
+                                 v-model="value"
+                                 :active="show"
+                                 :indeterminate="query"
+                                 :query="true"
+              ></v-progress-linear>
+            </v-row>
+
             <v-row class="pb-3 px-5">
               <GeneralStage
                 :stage="stage"
@@ -119,11 +128,21 @@ export default {
     hiker_lat: 33.933729,
     MACHomePointExists: false,
     ERUDropPointExists: false,
+    value: 0,
+    query: false,
+    show: true,
+    interval: 0
   }),
 
   mounted() {
     // setTimeout(this.getCurrentData, 5000);
+    this.queryAndIndeterminate()
   },
+
+  beforeDestroy () {
+    clearInterval(this.interval)
+  },
+
   updated() {
     if (!this.firstGetMAC && !this.firstGetHiker) {
       this.getCurrentData();
@@ -242,6 +261,24 @@ export default {
     },
     addERUDrop(lng, lat) {
       this.$refs.Map.addCoord("eru_drop_loc", "drop-location", lng, lat); //not sure if naming is correct
+    },
+    queryAndIndeterminate () {
+      this.query = true
+      this.show = true
+      this.value = 0
+
+      setTimeout(() => {
+        this.query = false
+
+        this.interval = setInterval(() => {
+          if (this.value === 100) {
+            clearInterval(this.interval)
+            this.show = false
+            return setTimeout(this.queryAndIndeterminate, 2000)
+          }
+          this.value += 25
+        }, 1000)
+      }, 2500)
     },
   },
 };
